f10dc1bbe5809ab875abbb9d3688f994e48d0ae0
Arthur De Witte <arthur.dewitte@gmail.com>
2020-07-30 14:26:21 +0200

    Added suggestions ect

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from os import name, system
 
 import discord
+from BeatPy.discord import formatter
 from discord.ext import commands
 
 # Windows and Linux compatibility
@@ -13,6 +14,10 @@ if name == "nt":
 # Reading our config.cfg file
 cfg = ConfigParser()
 cfg.read("config.cfg")
+
+# Setup BeatPy
+setup = formatter.Embed(footer=True, footer_message=f"©Xiler | {datetime.now().year}")
+em = setup.create
 
 
 # Our general logger:
@@ -64,48 +69,6 @@ class Logger:
 logger = Logger()
 
 
-# Setting up our bot object:
-class TempBot(commands.Bot):
-    def __init__(self):
-        system(clear)
-        logger.warn("Initializing system...")
-        super().__init__(command_prefix=commands.when_mentioned_or(cfg["GENERAL"].get("prefix")),
-                         description="The temporary bot for Xiler",
-                         case_insensitive=True,
-                         help_attrs=dict(hidden=True))
-
-        # Add the COG's
-        self.add_cog(EventListener(self))
-
-    # Start the bot
-    def run(self):
-        super().run(cfg["GENERAL"].get("token"), reconnect=True)
-
-    # Our introduction message
-    async def on_ready(self):
-        system(clear)
-        logger.log(f"Successfully initialized on {self.user.name}!")
-        print(f"\n\n{logger.red + logger.bold}LEGAL NOTICE:\n"
-              f"This bot is official property of Xiler (https://www.xiler.net) and any of its ToS or Privacy policies "
-              f"apply to this application!{logger.default + logger.normal}\n\n")
-
-    # Handle messages properly (prevent bot response, log, process)
-    async def on_message(self, message):
-        if message.author.bot:
-            return
-        ctx = await self.get_context(message)
-        if ctx.valid:
-            logger.command_executed(ctx)
-        await self.process_commands(message)
-
-    # Prevent unknown exception errors
-    async def on_command_error(self, ctx, exception):
-        if isinstance(type(exception), discord.ext.commands.errors.CommandNotFound):
-            return
-        else:
-            logger.error(str(exception))
-
-
 # Handles all events:
 class EventListener(commands.Cog):
     def __init__(self, bot: discord.Client):
@@ -128,6 +91,96 @@ class EventListener(commands.Cog):
     async def on_member_remove(self, member: discord.Member):
         if member.guild.id == 696758091768791080:  # Because this script is temporary we'll only allow this in our guild
             logger.guild_join_leave(member)
+
+
+# The class for all other commands:
+class GeneralCommands(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.guild = 696758091768791080
+        self.suggestions = 703192340998651909  # 703192340998651909
+        # self.cogs = self.bot.cogs
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        self.guild = self.bot.get_guild(self.guild)
+        self.suggestions = self.guild.get_channel(self.suggestions)
+
+    @commands.command(name="reload", aliases=["rl"])
+    @commands.has_role(699699122432639117)
+    async def reload(self, ctx):
+        msg = await ctx.send("Started reloading")
+        for cog in self.bot.cogs:
+            self.bot.remove_cog(cog.__name__)
+            self.bot.add_cog(cog(self.bot))
+        await msg.delete()
+        await ctx.send(f"~~{msg.content}~~\nFinished!")
+
+    @commands.command(name="suggest", aliases=["sug"])
+    async def suggest(self, ctx, *, suggestion: str):
+        if ctx.guild == self.guild:
+            if isinstance(self.suggestions, int):
+                await self.on_ready()
+            msg = await self.suggestions.send(**em(suggestion,
+                                                   footer_message=f"Submitted by {ctx.author} ({ctx.author.id})",
+                                                   footer_icon=ctx.author.avatar_url))
+            for emoji in ["👍", "😐", "👎"]:
+                await msg.add_reaction(emoji)
+            await ctx.send(
+                **em(f"Successfully send your suggestion in {self.suggestions.mention}!\n``` {suggestion} ```",
+                     color=0xff0016, timestamp=False))
+
+
+# Setting up our bot object:
+class TempBot(commands.Bot):
+    cogs = [EventListener, GeneralCommands]
+
+    def __init__(self):
+        system(clear)
+        logger.warn("Initializing system...")
+        super().__init__(command_prefix=commands.when_mentioned_or(cfg["GENERAL"].get("prefix")),
+                         description="The temporary bot for Xiler",
+                         case_insensitive=True,
+                         help_attrs=dict(hidden=True))
+
+        # Add the COG's
+        print("\n")
+        for cog in self.cogs:
+            logger.log(
+                f"{logger.cyan}Started{logger.default} loading {logger.yellow + cog.__name__ + logger.default}...")
+            self.add_cog(cog(self))
+            logger.log(
+                f"{logger.cyan}Successfully{logger.default} loaded {logger.yellow + cog.__name__ + logger.default}!")
+        print("\n")
+
+    # Start the bot
+    def run(self):
+        super().run(cfg["GENERAL"].get("token"), reconnect=True)
+
+    # Our introduction message
+    async def on_ready(self):
+        logger.log(f"Successfully initialized on {self.user.name}!")
+        print(f"\n\n{logger.red + logger.bold}LEGAL NOTICE:\n"
+              f"This bot is official property of Xiler (https://www.xiler.net) and any of its ToS or Privacy policies "
+              f"apply to this application!{logger.default + logger.normal}\n\n")
+
+    # Handle messages properly (prevent bot response, log, process)
+    async def on_message(self, message):
+        if message.author.bot:
+            return
+        ctx = await self.get_context(message)
+        if ctx.valid:
+            logger.command_executed(ctx)
+        await self.process_commands(message)
+
+    # Prevent unknown exception errors
+    async def on_command_error(self, ctx, exception):
+        if isinstance(exception, (commands.errors.CommandNotFound,)):
+            return
+        else:
+            message = f"{type(exception).__name__}: {exception}".capitalize()
+            logger.error(message)
+            await ctx.send(**em(message, title="Oops...", color=0x0000ff))
 
 
 if __name__ == "__main__":
